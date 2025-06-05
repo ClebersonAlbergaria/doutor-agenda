b14032d990b4dce842a530d17b22e84a50fb043e
ClebersonAlbergaria <48031539+ClebersonAlbergaria@users.noreply.github.com>
2025-06-04 22:09:37 -0300

    feat: add functions on dashboard views

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,29 +1,28 @@
 import dayjs from "dayjs";
-import { and, count, eq, gte, lte, sum } from "drizzle-orm";
-import { Calendar, DollarSign, Stethoscope, Users } from "lucide-react";
+import { Calendar } from "lucide-react";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataTable } from "@/components/ui/data-table";
 import {
   PageActions,
+  PageContainer,
   PageContent,
   PageHeader,
   PageHeaderContent,
   PageSubtitle,
   PageTitle,
 } from "@/components/ui/page-conteiner";
-import { PageContainer } from "@/components/ui/page-conteiner";
-import { db } from "@/db";
-import {
-  appointmentsTable,
-  doctorsTable,
-  patientsTable,
-  usersToClinicsTable,
-} from "@/db/schema";
+import { getDashboard } from "@/data/get-dashboard";
 import { auth } from "@/lib/auth";
 
+import { appointmentsTableColumns } from "../appointments/_components/table-columns";
 import { DatePicker } from "./components/date-picker";
-import { StatusCard } from "./components/status-card";
+import AppointmentsChart from "./components/revenue-chart";
+import StatsCards from "./components/status-card";
+import TopDoctors from "./components/top-doctors";
+import TopSpecialties from "./components/top-specialties";
 
 interface DashboardPageProps {
   searchParams: Promise<{
@@ -43,101 +42,73 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
     redirect("/clinic-form");
   }
 
-  const clinics = await db.query.usersToClinicsTable.findMany({
-    where: eq(usersToClinicsTable.userId, session.user.id),
-  });
-  if (clinics.length === 0) {
-    redirect("/clinic-form");
-  }
-
   const { from, to } = await searchParams;
   if (!from || !to) {
     redirect(
       `/dashboard?from=${dayjs().format("YYYY-MM-DD")}&to=${dayjs().add(1, "month").format("YYYY-MM-DD")}`,
     );
   }
-
-  const [[totalRevenue], [totalAppointments], [totalPatients], [totalDoctors]] =
-    await Promise.all([
-      db
-        .select({
-          total: sum(appointmentsTable.appointmentPriceInCents),
-        })
-        .from(appointmentsTable)
-        .where(
-          and(
-            eq(appointmentsTable.clinicId, session.user.clinic.id),
-            gte(appointmentsTable.date, new Date(from)),
-            lte(appointmentsTable.date, new Date(to)),
-          ),
-        ),
-      db
-        .select({
-          total: count(),
-        })
-        .from(appointmentsTable)
-        .where(
-          and(
-            eq(appointmentsTable.clinicId, session.user.clinic.id),
-            gte(appointmentsTable.date, new Date(from)),
-            lte(appointmentsTable.date, new Date(to)),
-          ),
-        ),
-      db
-        .select({
-          total: count(),
-        })
-        .from(patientsTable)
-        .where(eq(patientsTable.clinicId, session.user.clinic.id)),
-      db
-        .select({
-          total: count(),
-        })
-        .from(doctorsTable)
-        .where(eq(doctorsTable.clinicId, session.user.clinic.id)),
-    ]);
-
-  const formatCurrency = (value: bigint | number | null) => {
-    if (!value) return "R$ 0";
-    return new Intl.NumberFormat("pt-BR", {
-      style: "currency",
-      currency: "BRL",
-    }).format(Number(value) / 100);
-  };
+  const {
+    totalRevenue,
+    totalAppointments,
+    totalPatients,
+    totalDoctors,
+    topDoctors,
+    topSpecialties,
+    todayAppointments,
+    dailyAppointmentsData,
+  } = await getDashboard({
+    from,
+    to,
+    session: {
+      user: {
+        clinic: {
+          id: session.user.clinic.id,
+        },
+      },
+    },
+  });
 
   return (
     <PageContainer>
       <PageHeader>
         <PageHeaderContent>
           <PageTitle>Dashboard</PageTitle>
-          <PageSubtitle>Gerencie os pacientes da sua clínica</PageSubtitle>
+          <PageSubtitle>Tenha uma visão geral da sua clínica.</PageSubtitle>
         </PageHeaderContent>
         <PageActions>
           <DatePicker />
         </PageActions>
       </PageHeader>
       <PageContent>
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-          <StatusCard
-            icon={<DollarSign className="text-primary h-4 w-4" />}
-            title="Faturamento"
-            value={formatCurrency(totalRevenue.total)}
-          />
-          <StatusCard
-            icon={<Calendar className="text-primary h-4 w-4" />}
-            title="Agendamentos"
-            value={totalAppointments.total ?? 0}
-          />
-          <StatusCard
-            icon={<Users className="text-primary h-4 w-4" />}
-            title="Pacientes"
-            value={totalPatients.total ?? 0}
-          />
-          <StatusCard
-            icon={<Stethoscope className="text-primary h-4 w-4" />}
-            title="Médicos"
-            value={totalDoctors.total ?? 0}
-          />
+        <StatsCards
+          totalRevenue={totalRevenue.total ? Number(totalRevenue.total) : null}
+          totalAppointments={totalAppointments.total}
+          totalPatients={totalPatients.total}
+          totalDoctors={totalDoctors.total}
+        />
+        <div className="grid grid-cols-[2.25fr_1fr] gap-4">
+          <AppointmentsChart dailyAppointmentsData={dailyAppointmentsData} />
+          <TopDoctors doctors={topDoctors} />
+        </div>
+        <div className="grid grid-cols-[2.25fr_1fr] gap-4">
+          <Card>
+            <CardHeader>
+              <div className="flex items-center gap-3">
+                <Calendar className="text-muted-foreground" />
+                <CardTitle className="text-base">
+                  Agendamentos de hoje
+                </CardTitle>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <DataTable
+                columns={appointmentsTableColumns}
+                data={todayAppointments}
+              />
+            </CardContent>
+          </Card>
+          <TopSpecialties topSpecialties={topSpecialties} />
         </div>
       </PageContent>
     </PageContainer>
